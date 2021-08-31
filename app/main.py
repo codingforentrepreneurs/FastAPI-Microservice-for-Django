3673eec0fcfd8ba277937437b222a16009e1d2a7
CFE <hello@teamcfe.com>
2021-08-31 17:07:07 -0500

    5 - FastAPI & Jinja Templates

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
-from fastapi import FastAPI
+import pathlib
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
+BASE_DIR = pathlib.Path(__file__).parent
 app = FastAPI()
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+# REST API
 
-
-@app.get("/") # http GET
-def home_view():
-    return {"hello": "world"}
+@app.get("/", response_class=HTMLResponse) # http GET -> JSON
+def home_view(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request, "abc": 123})
 
 
 @app.post("/") # http POST
