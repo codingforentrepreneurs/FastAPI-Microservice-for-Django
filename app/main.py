a4dce36b42ea0b38e08085e1ed0cea33e97f5ba0
CFE <hello@teamcfe.com>
2021-09-03 15:44:02 -0500

    10 - FastAPI Settings & Environment Variables & dotenv

--- app/main.py
+++ app/main.py
@@ -1,15 +1,33 @@
 import pathlib
-from fastapi import FastAPI, Request
+import os
+from functools import lru_cache
+from fastapi import FastAPI, Depends, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    debug: bool = False
+
+    class Config:
+        env_file = ".env"
+
+@lru_cache
+def get_settings():
+    return Settings()
+
+settings = get_settings()
+DEBUG=settings.debug
 
 BASE_DIR = pathlib.Path(__file__).parent
 app = FastAPI()
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 # REST API
 
+print(DEBUG)
 @app.get("/", response_class=HTMLResponse) # http GET -> JSON
-def home_view(request: Request):
+def home_view(request: Request, settings:Settings = Depends(get_settings)):
+    print(settings.debug)
     return templates.TemplateResponse("home.html", {"request": request, "abc": 123})
 
 
