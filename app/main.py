d2069e86a3cfe4fa884f20be582867c94e5438de
CFE <hello@teamcfe.com>
2021-09-03 16:06:13 -0500

    11 - Handling File Uploads

--- app/main.py
+++ app/main.py
@@ -1,13 +1,23 @@
 import pathlib
 import os
+import io
+import uuid
 from functools import lru_cache
-from fastapi import FastAPI, Depends, Request
-from fastapi.responses import HTMLResponse
+from fastapi import(
+    FastAPI,
+    HTTPException,
+    Depends,
+    Request,
+    File,
+    UploadFile
+    )
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     debug: bool = False
+    echo_active: bool = False
 
     class Config:
         env_file = ".env"
@@ -20,17 +30,29 @@ settings = get_settings()
 DEBUG=settings.debug
 
 BASE_DIR = pathlib.Path(__file__).parent
+UPLOAD_DIR = BASE_DIR / "uploaded"
 app = FastAPI()
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
-# REST API
 
-print(DEBUG)
+
 @app.get("/", response_class=HTMLResponse) # http GET -> JSON
 def home_view(request: Request, settings:Settings = Depends(get_settings)):
-    print(settings.debug)
     return templates.TemplateResponse("home.html", {"request": request, "abc": 123})
 
 
 @app.post("/") # http POST
 def home_detail_view():
     return {"hello": "world"}
+
+
+@app.post("/img-echo/", response_class=FileResponse) # http POST
+async def img_echo_view(file:UploadFile = File(...), settings:Settings = Depends(get_settings)):
+    if not settings.echo_active:
+        raise HTTPException(detail="Invalid endpoint", status_code=400)
+    bytes_str = io.BytesIO(await file.read())
+    fname = pathlib.Path(file.filename)
+    fext = fname.suffix # .jpg, .txt
+    dest = UPLOAD_DIR / f"{uuid.uuid1()}{fext}"
+    with open(str(dest), 'wb') as out:
+        out.write(bytes_str.read())
+    return dest
